b97419d2736418410fc0b229feecb256d0b7d024
sebmartinezpedemonte <sebastianmartinezpedemonte@gmail.com>
2022-11-15 21:25:09 -0300

    cambios en estilos con bootstrap y vistas

--- PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB/Models/PropiedadAlquiler.cs
+++ PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB/Models/PropiedadAlquiler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
+using PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB.Models;
 
 namespace PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB
 {
@@ -15,5 +16,12 @@ namespace PNT1_GRUPO6_PROYECTO_INMOBILIARIA_WEB
         {
             throw new NotImplementedException();
         }
+
+        //agregar usuario como nullable (poniendo ? adelante del tipo)
+        
+        //public int? IdUsuario { get; set; }
+        
+        //public Usuario usuario { get; set; }
+        
     }
 }
